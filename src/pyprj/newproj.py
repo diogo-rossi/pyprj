a5a39dc5eacd7c895d81c0daa9abf5e81efac8cd
Diogo Rossi <rossi.diogo@gmail.com>
2026-02-18 20:47:33 -0300

    ✨ feat: add newproj module with command functions

---
+++ src/pyprj/newproj.py
@@ -0,0 +1,174 @@
+import os
+import sys
+
+GITIGNORE: str = """# Python-generated files
+__pycache__/
+*.py[oc]
+build/
+dist/
+wheels/
+*.egg-info
+
+# Virtual environments
+.venv
+"""
+
+PYPROJECT_EXTRALINES: str = r"""
+[tool.pytest]
+addopts = [
+    "-rx",        # Show extra test summary: (x)failed
+    "-vv",        # Increase verbosity
+    "-s",         # Shortcut for --capture=no (Per-test capturing method)
+    "--tb=short", # Traceback print mode = short
+]
+
+[tool.black]
+line-length = {{line_length}}
+
+[tool.taskipy.tasks]
+docs = { cmd = "make html", cwd = "./doc/sphinx", help = "Make docs with sphinx in folder './doc/sphinx'" }
+open = { cmd = "index.html", cwd = "./doc/sphinx/build/html", help = "Open sphinx docs in folder './doc/sphinx/build/html'" }
+test = { cmd = "pytest", cwd = "./tests", help = "Test with pytest in folder './tests'" }
+"""
+
+SETTINGS_JSON: str = r"""{
+    "[python]": {
+        "editor.rulers": [
+            {
+                "column": {{line_length}},
+                "color": "#36a3f0"
+            }
+        ],
+        "rewrap.wrappingColumn": 0
+    },
+    "black-formatter.args": [
+        "--line-length",
+        "{{line_length}}"
+    ],
+    "isort.args": [
+        "--line-length",
+        "{{line_length}}"
+    ],
+    "python.testing.pytestArgs": [
+        "tests",
+    ],
+    "python.testing.unittestEnabled": false,
+    "python.testing.pytestEnabled": true,
+    "editor.formatOnSave": true,
+    "editor.formatOnType": true,
+    "editor.codeActionsOnSave": {
+        "source.organizeImports": "always"
+    },
+}"""
+
+TASKS_JSON = r"""{
+    // See https://go.microsoft.com/fwlink/?LinkId=733558
+    // for the documentation about the tasks.json format
+    "version": "2.0.0",
+    "tasks": [
+        {
+            "label": "Make sphinx docs with taskipy in folder './docs/sphinx'",
+            "type": "shell",
+            "command": "uv run task docs",
+        },
+        {
+            "label": "Run pytest with taskipy in folder './tests'",
+            "type": "shell",
+            "command": "uv run task test",
+        },
+        {
+            "label": "Open docs with taskipy in folder './docs/sphinx/build/html'",
+            "type": "shell",
+            "command": "uv run task open",
+        }
+    ]
+}"""
+
+
+def ini(
+    python: str = "3.12",
+    black_line_length: int = 128,
+):
+    """Create a new project for a python package.
+
+    Parameters
+    ----------
+    - `python` (`str`, optional): Defaults to `"3.12"`.
+        The Python interpreter to use to determine the minimum supported Python version.
+
+    - `black_line_length` (`int`, optional): Defaults to `128`.
+        Line length parameter to use with `black`.
+    """
+
+    cmd: str = f"git init"
+    msg: str = f"> running comand: `{cmd}`"
+    sep: str = "-" * len(msg)
+    print(f"{sep}\n{msg}\n{sep}")
+    exit_code = os.system(cmd)
+    if exit_code != 0:
+        sys.exit(exit_code)
+
+    if "PYPRJ_AUTHOR" in os.environ:
+        author = os.environ["PYPRJ_AUTHOR"]
+        cmd: str = f'git config --local user.name "{author}"'
+        msg: str = f"> running git comand: `{cmd}`"
+        sep: str = "-" * len(msg)
+        print(f"{sep}\n{msg}")
+        exit_code = os.system(cmd)
+        if exit_code != 0:
+            sys.exit(exit_code)
+
+    if "PYPRJ_EMAIL" in os.environ:
+        email = os.environ["PYPRJ_EMAIL"]
+        cmd: str = f'git config --local user.email "{email}"'
+        msg: str = f"> running git comand: `{cmd}`"
+        sep: str = "-" * len(msg)
+        print(f"{msg}")
+        exit_code = os.system(cmd)
+        if exit_code != 0:
+            sys.exit(exit_code)
+
+    cmd: str = f"uv init --author-from git --python {python} --lib"
+    msg: str = f"> running uv comand: `{cmd}`"
+    sep: str = "-" * len(msg)
+    print(f"{sep}\n{msg}\n{sep}")
+    exit_code = os.system(cmd)
+    if exit_code != 0:
+        sys.exit(exit_code)
+
+    cmd: str = f"uv add --dev black pytest taskipy"
+    msg: str = f"> Adding 'dev' packages, running uv comand: `{cmd}`"
+    sep: str = "-" * len(msg)
+    print(f"{sep}\n{msg}\n{sep}")
+    exit_code = os.system(cmd)
+    if exit_code != 0:
+        sys.exit(exit_code)
+
+    msg: str = f"> creating '.gitignore' file"
+    sep: str = "-" * len(msg)
+    print(f"{sep}\n{msg}")
+    with open(".gitignore", "w", encoding="utf-8") as file:
+        file.write(GITIGNORE)
+
+    msg: str = f"> editing 'pyproject.toml' file"
+    print(f"{msg}")
+    with open("pyproject.toml", "a", encoding="utf-8") as file:
+        file.write(PYPROJECT_EXTRALINES.replace("{{line_length}}", str(black_line_length)))
+
+    msg: str = f"> creating '.vscode' folder"
+    print(f"{msg}")
+    try:
+        os.mkdir(".vscode")
+    except:
+        print(f"{sep}")
+        raise
+
+    msg: str = f"> creating '.vscode/settings.json' file"
+    print(f"{msg}")
+    with open(".vscode/settings.json", "w", encoding="utf-8") as file:
+        file.write(SETTINGS_JSON.replace("{{line_length}}", str(black_line_length)))
+
+    msg: str = f"> creating '.vscode/tasks.json' file"
+    print(f"{msg}")
+    with open(".vscode/tasks.json", "w", encoding="utf-8") as file:
+        file.write(TASKS_JSON)
