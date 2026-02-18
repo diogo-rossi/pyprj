df7fc9c340ec4dd2ab2b8d07ea65513d34f8eac9
Diogo Rossi <rossi.diogo@gmail.com>
2026-02-18 16:20:20 -0300

    ✨ feat: improve CLI and help messages

--- src/pyprj/cli.py
+++ src/pyprj/cli.py
@@ -1,12 +1,18 @@
-from clig import Command
+import shutil
+import textwrap
+from functools import partial
+
+from clig import Command, Context
 
 from pyprj import nbex, nbmd
 from pyprj.newproj import init as initproj
 
 
-def pyprj():
+def pyprj(ctx: Context):
     """CLI to manage python projects"""
-    print(locals())
+    subcommand = list(vars(ctx.namespace).values())[0]
+    if not subcommand:
+        ctx.command.run(["--help"])
 
 
 def docs():
@@ -28,16 +34,40 @@ def optmetavarmodifier(name: str):
     return f"<{name}>"
 
 
-kwargs = {"make_shorts": True, "optmetavarmodifier": optmetavarmodifier}
-# kwargs.pop("optmetavarmodifier")
+def format_help(
+    text: str,
+    width: int | None = None,
+    space: int = 24,
+    dedent: bool = True,
+    final_newlines: bool = False,
+    append_text: str = "",
+) -> str:
+    text = f"{text}{append_text}"
+    width = width or shutil.get_terminal_size().columns
+    lines = []
+    for line in text.splitlines():
+        line = textwrap.dedent(line) if dedent else line
+        lines.append(textwrap.fill(text=line, width=width - space, replace_whitespace=False))
+
+    return "\n".join(lines) + ("\n\n" if final_newlines else "")
+
+
+opthelpmodifier = partial(format_help, width=80, append_text="\nDefaults to '%(default)s'.")
+
+
+kwargs = {
+    "make_shorts": True,
+    "optmetavarmodifier": optmetavarmodifier,
+    "opthelpmodifier": opthelpmodifier,
+    "help_msg": format_help("Show this help message and exit.", width=80),
+}
 
 
 def main():
     # fmt: off
     cmd: Command = (
-        Command(pyprj, make_shorts=True)
-            .add_subcommand(initproj)
-            .add_subcommand(docs)
+        Command(pyprj, **kwargs)
+            .add_subcommand(initproj, **kwargs)
             .new_subcommand(docs)
                 .add_subcommand(init, **kwargs) 
                 .add_subcommand(nbmd, make_shorts=True)
