943a911fd72cd4284dc6b7b3491c0e00ce944ae3
Diogo Rossi <rossi.diogo@gmail.com>
2026-02-19 03:14:22 -0300

    📄 docs: add docstrings

--- src/pyprj/docman.py
+++ src/pyprj/docman.py
@@ -157,13 +157,18 @@ TREE: str = f"""
 
 
 def docs(ctx: Context):
-    """Manage documentation"""
+    """Manage documentation of the project.
+
+    If called without subcommands, runs the task 'docs' inside the project.
+    Tasks use the tool 'taskipy'. Currently are run with the tool 'uv.'
+    The task 'docs' makes docs with 'sphinx' in folder './doc/sphinx'.
+    """
     if vars(ctx.namespace)[ctx.command.sub_commands["docs"].subparsers_dest] is None:
         run_task("docs")
 
 
 def init():
-    """Initialize documentation"""
+    """Initialize documentation folder with packages."""
 
     from .pyproject import author_name, pkg_name
 
