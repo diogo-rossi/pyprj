8a79deb03aea7a55a9c2c3bcbf1a8c9707d754e3
Diogo Rossi <rossi.diogo@gmail.com>
2026-02-19 01:28:19 -0300

    ✅ test: change folder in resources

--- tests/resources.py
+++ tests/resources.py
@@ -5,7 +5,7 @@ from pyprj.cellfuncs import Notebook
 
 THIS_DIR = Path(__file__).parent
 
-notebook_filepath: Path = THIS_DIR / "notebookexample.ipynb"
+notebook_filepath: Path = THIS_DIR / "notebooks" / "notebookexample.ipynb"
 with open(notebook_filepath, "r", encoding="utf-8") as file:
     notebook: Notebook = json.load(file)
 
