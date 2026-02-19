0ef11579d46aa860cb2e7c47bef305cf84cb38c3
Diogo Rossi <rossi.diogo@gmail.com>
2026-02-19 03:15:48 -0300

    📄 docs: add doc to test subcommand

--- src/pyprj/taskscmd.py
+++ src/pyprj/taskscmd.py
@@ -13,4 +13,10 @@ def run_task(task: str):
 
 
 def test():
+    """Run task 'test' inside the project.
+
+    This command only runs the task 'test' inside the project.
+    Tasks use the tool 'taskipy'. Currently are run with the tool 'uv.'
+    The task 'test' runs tests with 'pytest' in folder './tests'.
+    """
     run_task("test")
