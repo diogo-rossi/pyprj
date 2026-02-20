e6d4b41adbbf91270cf1125c272ec77dc6c6a9a3
Diogo Rossi <rossi.diogo@gmail.com>
2026-02-20 18:40:50 -0300

    ✨ feat: add publish subcommand

---
+++ src/pyprj/publish.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+
+from .pyproject import pyproject
+from .run_cmd import run_cmd
+
+
+def publish():
+    """Publish package to PyPI
+
+    Uses token from file '.vscode/pyprj.json'
+    """
+    from .pyprj_json_data import json_content, published_versions, save_content, token
+    from .pyproject import version
+
+    if version in published_versions:
+        print(f"\nThe version '{version}' has already been published. Please, update it first.\n")
+        return
+
+    if not token:
+        print("\nPlease, provide a token in the file '.vscode/pyprj.json'\n")
+        return
+
+    error_code: int = run_cmd(f"uv publish {(pyproject.dirpath / 'dist/*').as_posix()} --token {token}", kind="uv")
+    if error_code == 0:
+        published_versions.append(version)
+        save_content(json_content)
