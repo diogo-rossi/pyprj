b103bce0d7dab95bce679ec6e1e4284940743415
Diogo Rossi <rossi.diogo@gmail.com>
2026-02-20 16:02:08 -0300

    🛠️ dev: new run_cmd module to run commands

---
+++ src/pyprj/run_cmd.py
@@ -0,0 +1,22 @@
+import os
+
+COLUMNS = 50
+try:
+    COLUMNS = os.get_terminal_size().columns
+except OSError:
+    pass
+
+
+SEP: str = "-" * COLUMNS
+
+
+def run_cmd(cmd: str, kind: str | None = None, add_sep: bool = True):
+    cmdkind: str = f"`{kind}` " if kind else ""
+    msg: str = f"> running {cmdkind}command: `{cmd}`"
+    sep: str = "-" * len(msg)
+    if add_sep:
+        print(f"{SEP}")
+    print(f"{msg}\n{sep}")
+    error_code = os.system(cmd)
+    print(SEP)
+    return error_code
