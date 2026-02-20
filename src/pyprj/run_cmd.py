27282bd468ec15bbff3465d83ac37c1ea1335351
Diogo Rossi <rossi.diogo@gmail.com>
2026-02-20 19:41:54 -0300

    🐞 fix: access pyproject.toml in run_cmd

--- src/pyprj/run_cmd.py
+++ src/pyprj/run_cmd.py
@@ -11,18 +11,17 @@ SEP: str = "-" * COLUMNS
 
 
 def run_cmd(cmd: str, kind: str | None = None, add_sep: bool = True):
-    from .pyproject import pyproject
-
-    pyproject_dirpath = pyproject.dirpath.as_posix()
 
     cmdkind: str = f"`{kind}` " if kind else ""
     msg: str = f"> running {cmdkind}command: `{cmd}`"
 
     if "--token" in msg:
         msg = f"{msg.split("--token")[0]} --token *****"
+        from .pyproject import pyproject
 
-    if pyproject_dirpath in msg:
-        msg = msg.replace(pyproject_dirpath, ".")
+        pyproject_dirpath = pyproject.dirpath.as_posix()
+        if pyproject_dirpath in msg:
+            msg = msg.replace(pyproject_dirpath, ".")
 
     sep: str = "-" * len(msg)
     if add_sep:
