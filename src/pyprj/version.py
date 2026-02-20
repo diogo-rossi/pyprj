703de34480d90765a68bc2278864ef831b84bec9
Diogo Rossi <rossi.diogo@gmail.com>
2026-02-20 18:41:04 -0300

    ✨ feat: add version subcommand

---
+++ src/pyprj/version.py
@@ -0,0 +1,19 @@
+from typing import Literal
+
+from clig import Arg, data
+
+from .run_cmd import run_cmd
+
+
+def version(semver_part: Arg[Literal["major", "minor", "patch"], data(nargs="?")]):
+    """Update or show project version
+
+    If called without 'semver' options, only show the project version.
+    """
+    if not semver_part:
+        from .pyproject import pkg_name, version
+
+        print(f"Package '{pkg_name}' version: {version}")
+        return
+
+    run_cmd(f"uv version --bump {semver_part}", kind="uv")
