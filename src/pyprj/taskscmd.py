31f7738431134874dfcac8464a9b79ed3889d6ab
Diogo Rossi <rossi.diogo@gmail.com>
2026-02-20 18:42:00 -0300

    ✨ feat: add build subcommand

--- src/pyprj/taskscmd.py
+++ src/pyprj/taskscmd.py
@@ -17,3 +17,13 @@ def test():
     The task 'test' runs tests with 'pytest' in folder './tests'.
     """
     run_task("test")
+
+
+def build():
+    """Run task 'build' inside the project.
+
+    This command only runs the task 'build' inside the project.
+    Tasks use the tool 'taskipy'. Currently are run with the tool 'uv'.
+    The task 'build' builds the package with 'uv' in root folder.
+    """
+    run_task("build")
