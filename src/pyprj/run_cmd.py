83ed954d9b95d33957285aaa4d815a4f7cfdd43f
Diogo Rossi <rossi.diogo@gmail.com>
2026-02-20 18:34:37 -0300

    🛠️ dev: treat run_cmd with paths and tokens

--- src/pyprj/run_cmd.py
+++ src/pyprj/run_cmd.py
@@ -1,5 +1,7 @@
 import os
 
+from .pyproject import pyproject
+
 COLUMNS = 50
 try:
     COLUMNS = os.get_terminal_size().columns
@@ -8,11 +10,19 @@ except OSError:
 
 
 SEP: str = "-" * COLUMNS
+pyproject_dirpath = pyproject.dirpath.as_posix()
 
 
 def run_cmd(cmd: str, kind: str | None = None, add_sep: bool = True):
     cmdkind: str = f"`{kind}` " if kind else ""
     msg: str = f"> running {cmdkind}command: `{cmd}`"
+
+    if "--token" in msg:
+        msg = f"{msg.split("--token")[0]} --token *****"
+
+    if pyproject_dirpath in msg:
+        msg = msg.replace(pyproject_dirpath, ".")
+
     sep: str = "-" * len(msg)
     if add_sep:
         print(f"{SEP}")
