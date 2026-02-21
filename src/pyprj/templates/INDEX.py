72c208f60bace4eaf9dc69fcba5eb62b8626bfee
Diogo Rossi <rossi.diogo@gmail.com>
2026-02-20 21:56:53 -0300

    🛠️ dev: add templates

---
+++ src/pyprj/templates/INDEX.py
@@ -0,0 +1,27 @@
+INDEX_MD = """# {pkg_name}
+
+Description of the project.
+
+## Installation
+
+```bash
+pip install {pkg_name}
+```
+
+```{{toctree}}
+:caption: Table of contents
+:maxdepth: 1
+
+userguide
+apireference
+```
+
+## Links
+
+- Source repository:
+  [{source_repo}]({source_repo})
+- PyPI:
+  [{pypi_project}]({pypi_project})
+- Documentation:
+  [{documentation_page}]({documentation_page})
+"""
