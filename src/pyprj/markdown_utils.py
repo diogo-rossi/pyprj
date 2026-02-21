d3a5526e667c382320a50c0716dae0bfefd7f382
Diogo Rossi <rossi.diogo@gmail.com>
2026-02-21 17:06:43 -0300

    ✨ feat: add markdown utils

---
+++ src/pyprj/markdown_utils.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+
+def get_markdown_sections(filepath: Path) -> list[str]:
+    with open(filepath, "r", encoding="utf-8") as file:
+        lines: list[str] = file.readlines()
+
+    sections: list[str] = [""]
+    for line in lines:
+        if line.startswith("#"):
+            sections.append(line)
+        else:
+            sections[-1] += line
+    return sections
