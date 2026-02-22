a794b5ac5e6287a9358faa286fe3416e4a5fbe90
Diogo Rossi <rossi.diogo@gmail.com>
2026-02-22 18:55:21 -0300

    ♻️ refactor: update help formatter

--- src/pyprj/cli.py
+++ src/pyprj/cli.py
@@ -54,14 +54,20 @@ def format_help(
     text = f"{text}{append_text}"
     width = width or shutil.get_terminal_size().columns
     lines = []
+    glue_lines = [""]
     for line in text.splitlines():
+        if not glue_lines[-1].endswith("."):
+            glue_lines[-1] = glue_lines[-1] + " " + line
+        else:
+            glue_lines.append(line)
+    for line in glue_lines:
         line = textwrap.dedent(line) if dedent else line
         lines.append(textwrap.fill(text=line, width=width - space, replace_whitespace=False))
 
     return "\n".join(lines) + ("\n\n" if final_newlines else "")
 
 
-opthelpmodifier = partial(format_help, width=95, append_text=" Defaults to '%(default)s'.")
+opthelpmodifier = partial(format_help, width=90, append_text="\nDefaults to '%(default)s'.")
 
 
 def optmetavarmodifier(name: str):
