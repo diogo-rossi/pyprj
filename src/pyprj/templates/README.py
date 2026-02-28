40003ae481bd5780ddb844871362a5d2f1d20409
Diogo Rossi <rossi.diogo@gmail.com>
2026-02-28 08:39:42 -0300

    ✨ feat: update README template

--- src/pyprj/templates/README.py
+++ src/pyprj/templates/README.py
@@ -1,4 +1,15 @@
 README_MD: str = r"""# {{pkg_name}}
 
 Description of the package.
+
+## Installation
+
+```sh
+pip install {{pkg_name}}
+```
+
+## License
+
+`{{pkg_name}}` is distributed under the terms of the
+[MIT](https://spdx.org/licenses/MIT.html) license.
 """
