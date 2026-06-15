d700b24be0798e40971bbf365f9cec12785c21a8
Diogo Rossi <rossi.diogo@gmail.com>
2026-06-15 16:29:58 -0300

    ✨ feat: move files before build in upver

--- src/pyprj/upver.py
+++ src/pyprj/upver.py
@@ -1,3 +1,4 @@
+import shutil
 import sys
 from pathlib import Path
 from typing import Literal
@@ -8,11 +9,12 @@ from .run_cmd import run_cmd
 from .taskscmd import build as buill_project
 
 
-def upver(semver: Arg[Literal["major", "minor", "patch"], data(nargs="?")], build: bool = False):
+def upver(semver: Arg[Literal["major", "minor", "patch"], data(nargs="?")], build: bool = False, move: bool = False):
     """Update or show project version.
 
     Currently, it uses the command `uv version`.
     If called without positional arguments, only show the project version.
+    .
 
     Parameters
     ----------
@@ -21,6 +23,9 @@ def upver(semver: Arg[Literal["major", "minor", "patch"], data(nargs="?")], buil
 
     - `build` (`bool`, optional): Defaults to `False`.
         Whether to build the project after update version.
+
+    - `move` (`bool`, optional): Defaults to `False`.
+        Whether to move files from `dist/` folder to `builds/` folder (before building).
     """
 
     if not semver:
@@ -30,16 +35,28 @@ def upver(semver: Arg[Literal["major", "minor", "patch"], data(nargs="?")], buil
         print(f"\nThe current package is in the version above.")
         print("To update the project version, use a positional argument in the `upver` subcommand:")
         print("> " + " | ".join(["major", "minor", "patch"]))
+        print()
         return error_code
 
     error_code = run_cmd(f"uv version --bump {semver}", kind="uv")
     if error_code != 0:
         sys.exit("\nNo `pyproject.toml` file found in this directory or parent directories.\nIt is not a project yet.\n")
 
+    from .pyproject import __get_pyproject_data, pkg_name, pyproject
+
+    if move:
+        dist_dir: Path = pyproject.dirpath / "dist"
+        if dist_dir.exists() and dist_dir.is_dir():
+            builds_dir: Path = pyproject.dirpath / "builds"
+            builds_dir.mkdir(parents=True, exist_ok=True)
+            for file_path in dist_dir.glob("*.*"):
+                shutil.move(str(file_path), str(builds_dir / file_path.name))
+                print("-------------------------")
+                print(f"Move file: - {file_path}")
+                print(f"To folder: - {builds_dir}")
+
     if build:
         buill_project()
-
-    from .pyproject import __get_pyproject_data, pkg_name, pyproject
 
     about_path: Path = Path(pyproject.dirpath / f"src/{pkg_name}/__about__.py")
     if about_path.exists():
